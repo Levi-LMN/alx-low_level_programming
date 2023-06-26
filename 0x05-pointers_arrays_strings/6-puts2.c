64cdec19e772ff750ca4dfe900f069c7293d17b1
Levi-LMN <Mukuhalevi@gmail.com>
2023-06-26 14:11:25 -0400

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 /**
- * puts2 - Prints every other character of a string, starting 1st
+ * puts2 - Prints every other character of a string, starting with the first character.
  * @str: Pointer to the string.
- * Return: none
  */
 void puts2(char *str)
 {
-	if (str != NULL)
-	{
-		int i = 0;
+    if (str != NULL)
+    {
+        int i = 0;
 
-		while (str[i] != '\0')
-		{
-			putchar(str[i]);
-			i++;
-		}
-	}
-	putchar('\n');
+        while (str[i] != '\0')
+        {
+            putchar(str[i]);
+
+            i++;
+
+            if (str[i] != '\0')
+                i++;
+        }
+    }
+
+    putchar('\n');
 }
 
