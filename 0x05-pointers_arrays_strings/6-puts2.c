b63f943afdc0f93b978699e431c4323a42019878
Levi-LMN <Mukuhalevi@gmail.com>
2023-06-26 13:59:44 -0400

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every other character of a string, starting 1st
+ * @str: Pointer to the string.
+ * Return: none
+ */
+void puts2(char *str)
+{
+	if (str != NULL)
+	{
+		int i = 0;
+
+		while (str[i] != '\0')
+		{
+			putchar(str[i]);
+			i += 2;
+		}
+	}
+	putchar('\n');
+}
+
