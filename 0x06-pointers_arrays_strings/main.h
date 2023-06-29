f4a4615fdc6fcbe26cb5165d7dc5bf5e72628086
Levi-LMN <Mukuhalevi@gmail.com>
2023-06-29 09:28:18 -0400

    strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strcat(char *dest, const char *src, int n);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, int n);
 int _putchar(char c);
 
 #endif /* MAIN_H */
