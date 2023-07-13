61056fa851a4f3869ab6b2033743ad2207758423
Levi-LMN <Mukuhalevi@gmail.com>
2023-07-14 01:54:37 +0300

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
 
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
-
-#endif /* MAIN_H */
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
 
