37d2cc8eedc4920c0db78e17e470d0d080ff7c4c
Levi-LMN <Mukuhalevi@gmail.com>
2023-07-17 20:32:45 +0300

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif /* DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
