9d0f5313ff8d08b27c27f1260d3cb9bf41f2a766
Levi-LMN <Mukuhalevi@gmail.com>
2023-08-01 12:18:06 +0300

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: Pointer to a pointer to the head of the list that is  to be freed.
+ * Levi's code
+ * Return: Null
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
+
