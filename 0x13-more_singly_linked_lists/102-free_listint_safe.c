5b98676683d5a6714e095dabf66b01adeac0d4c9
Levi-LMN <Mukuhalevi@gmail.com>
2023-08-02 02:10:51 +0300

    advanced tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,65 +3,62 @@
 #include <stdio.h>
 
 /**
- * _realloc_list - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @old_list: the old list to append
- * @new_size: size of the new list (always one more than the old list)
- * @new_node: new node to add to the list
- *
- * Return: pointer to the new list
+ * real_loc - reallocates memory for an array of pointers
+ * @list: the old list
+ * @size: size of the new list
+ * @new: new node to add to the list
+ * Levi's code
+ * Return: pointer to new list
  */
-listint_t **_realloc_list(listint_t **old_list, size_t new_size, listint_t *new_node)
+listint_t **real_loc(listint_t **list, size_t size, listint_t *new)
 {
-    listint_t **new_list;
-    size_t i;
+	listint_t **newlist;
+	size_t i;
 
-    new_list = malloc(new_size * sizeof(listint_t *));
-    if (new_list == NULL)
-    {
-        free(old_list);
-        exit(98);
-    }
-    for (i = 0; i < new_size - 1; i++)
-        new_list[i] = old_list[i];
-    new_list[i] = new_node;
-    free(old_list);
-    return (new_list);
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
 }
 
 /**
  * free_listint_safe - frees a listint_t linked list.
- * @h: double pointer to the start of the list
- *
- * Return: the number of nodes in the list
+ * @head: double pointer to the start
+ * Levi's code
+ * Return: no. of nodes in the list
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
-    size_t i, num_nodes = 0;
-    listint_t **list = NULL;
-    listint_t *next_node;
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-    if (h == NULL || *h == NULL)
-        return (num_nodes);
-
-    while (*h != NULL)
-    {
-        for (i = 0; i < num_nodes; i++)
-        {
-            if (*h == list[i])
-            {
-                *h = NULL;
-                free(list);
-                return (num_nodes);
-            }
-        }
-        num_nodes++;
-        list = _realloc_list(list, num_nodes, *h);
-        next_node = (*h)->next;
-        free(*h);
-        *h = next_node;
-    }
-    free(list);
-    return (num_nodes);
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = real_loc(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
 }
-
