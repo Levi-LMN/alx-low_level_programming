2fd3411adbe23cc692e4bce377b305c536513548
Levi-LMN <Mukuhalevi@gmail.com>
2023-08-01 10:48:04 +0300

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Prints all elements of a listint_t list.
+ * @h: Pointer to the head of the list.
+ * Levi's code
+ * Return: The no. of nodes in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
+
